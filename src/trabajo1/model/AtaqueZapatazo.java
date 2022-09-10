3dcc023e84921eba5e184f675dd448c7f3f565de
Tomas Miranda <tomasmiranda98@hotmail.com>
2022-09-10 12:20:41 -0300

    se soluciona random en ataque zapato

--- src/trabajo1/model/AtaqueZapatazo.java
+++ src/trabajo1/model/AtaqueZapatazo.java
@@ -1,6 +1,9 @@
 package trabajo1.model;
 
+import java.util.Random;
+
 public class AtaqueZapatazo extends AtaqueDanio{
+    Random random = new Random();
     public AtaqueZapatazo(int valorDeAtaque) {
         super(valorDeAtaque);
     }
@@ -8,8 +11,16 @@ public class AtaqueZapatazo extends AtaqueDanio{
     @Override
     public void generarEfecto(Chinpokomon chinpokomon1, Chinpokomon chinpokomon2) {
         super.generarEfecto(chinpokomon1, chinpokomon2);
+        /*
         var aux = Math.random() * ( 3 - 1 );
         if(aux == 1) {
+            super.generarEfecto(chinpokomon1, chinpokomon2);
+        }
+
+         */
+        //devuelve entre 0(inclusivo) y el valor pasado(Exclusivo) [0;n) || [O;n-1]
+        int randomIndex = random.nextInt(2);
+        if(randomIndex==1){
             super.generarEfecto(chinpokomon1, chinpokomon2);
         }
     }
