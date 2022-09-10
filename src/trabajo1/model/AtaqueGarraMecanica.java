0a29f407269eb3d36310de8b9eec48ed805dbd58
Tomas Miranda <tomasmiranda98@hotmail.com>
2022-09-10 15:36:29 -0300

    se establece bien la logica de ataque de garra mecanica

--- src/trabajo1/model/AtaqueGarraMecanica.java
+++ src/trabajo1/model/AtaqueGarraMecanica.java
@@ -1,17 +1,29 @@
 package trabajo1.model;
 
+import java.util.Random;
+
 public class AtaqueGarraMecanica extends AtaqueDanio{
+    Random random = new Random();
     public AtaqueGarraMecanica(int valorDeAtaque) {
         super(valorDeAtaque);
     }
 
     @Override
     public void generarEfecto(Chinpokomon chinpokomon1, Chinpokomon chinpokomon2) {
-        super.generarEfecto(chinpokomon1, chinpokomon2);
-        if(Math.random() * ( 11 - 1 ) == 1) {
-            super.generarEfecto(chinpokomon1, chinpokomon2);
-        } else {
+        int random = this.generarRandom(10);
+        if(random == 1){
+            this.sacarMitadDeLaVidaQueLeQueda(chinpokomon2);
+        }else{
             super.generarEfecto(chinpokomon1, chinpokomon2);
         }
+    }
+    private int generarRandom(int valorMaximoExcluyente){
+        return random.nextInt(valorMaximoExcluyente);
+    }
+    private int obtenerVidaRestanteDeChinpokomon(Chinpokomon chinpokomon){
+        return chinpokomon.getVida();
+    }
+    private void sacarMitadDeLaVidaQueLeQueda(Chinpokomon chinpokomon){
+        chinpokomon.setVida(this.obtenerVidaRestanteDeChinpokomon(chinpokomon) - this.obtenerVidaRestanteDeChinpokomon(chinpokomon)/2);
     }
 }
